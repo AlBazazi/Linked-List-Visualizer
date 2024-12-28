42d00c4fbe7a181bd60f4b54e7976089aece846f
AlBazazi <168605550+AlBazazi@users.noreply.github.com>
2024-12-28 07:31:05 +0500

    Update Operations.java

--- src/Operations.java
+++ src/Operations.java
@@ -5,74 +5,80 @@ public class Operations {
 	public static int searchElement(LinkedList<Node> list) {
 		String strKey = JOptionPane.showInputDialog("Enter INFO to Search");
         	int key = Integer.parseInt(strKey);
+        	int index=-1;
     		for(int i=0;i<list.size();i++) {
-	    		if(list.get(i).info==key) return i;
-	    	}
-    		return -1;
+    			if(list.get(i).info==key) index=i;
+    		}
+    		if(index==-1) {
+    			JOptionPane.showMessageDialog(null, "Element NOT FOUND");
+    		}
+    		else {
+    			JOptionPane.showMessageDialog(null, "Element FOUND at Index "+index);
+    		}
 	}
 	
 	// REMOVE
 	
 	// ADD
 	public static LinkedList<Node> addElement(LinkedList<Node> list) {
-	    String[] options = {"Insert at Beginning", "Insert at End", "Insert by Index"};
-	    int choice = JOptionPane.showOptionDialog(null, "Choose Insertion method:",
+		String[] options = {"Insert at Beginning", "Insert at End", "Insert by Index"};
+	    	int choice = JOptionPane.showOptionDialog(null, "Choose Insertion method:",
 	            "Insert Node", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
 	            null, options, options[0]);
-	    if (choice==0) {
-	        String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
-	        int info=Integer.parseInt(i);
-	        int address=Visualizer.generateAddress();
-	        Node newFirstNode=new Node(info, address);
-	        newFirstNode.setNext(Visualizer.startLink);
-	        Visualizer.startLink = address;
-	        list.addFirst(newFirstNode);
-	    } 
-	    else if (choice==1) {
-	        String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
-	        int info=Integer.parseInt(i);
-	        int address=Visualizer.generateAddress();
-	        Node newLastNode=new Node(info, address);
-	        if (!list.isEmpty()) {
-	            list.getLast().setNext(address); // Link the new node to the last node
-	        }
-	        list.addLast(newLastNode);
-	    } 
-	    else {
-	        try {
-	            String j=JOptionPane.showInputDialog(null, "Enter Index of new Node");
-	            int index=Integer.parseInt(j);
-	            if (index>=0 && index<=list.size()) {
-	                String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
-	                int info=Integer.parseInt(i);
-	                int address=Visualizer.generateAddress();
-	                Node newNode=new Node(info, address);
-	                if(index==0) {
-	                	newNode.nextLink=Visualizer.startLink;
-	                	Visualizer.startLink=address;
-	                	list.addFirst(newNode);
-	                }
-	                else if(index==list.size()-1) {
-	                	list.getLast().nextLink=address;
-	                	list.addLast(newNode);
-	                }
-	                else {
-	                	Node current=list.get(index);
-	                	Node previous=list.get(index-1);
-	                	newNode.setNext(current.link);
-	                	previous.setNext(address);
-	                	list.add(index, newNode);
-	                }
-	            } 
-	            else {
-	                JOptionPane.showMessageDialog(null, "Index out of bounds.");
-	            }
-	        } 
-	        catch (NumberFormatException e) {
-	            JOptionPane.showMessageDialog(null, "Enter a valid integer.");
-	        }
-	    }
-	    return list;
+	    	if (choice==0) {
+	        	String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
+	        	int info=Integer.parseInt(i);
+	        	int address=Visualizer.generateAddress();
+	        	Node newFirstNode=new Node(info, address);
+	        	newFirstNode.setNext(Visualizer.startLink);
+	        	Visualizer.startLink = address;
+	        	list.addFirst(newFirstNode);
+	    	}	 
+	    	else if (choice==1) {
+	        	String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
+	        	int info=Integer.parseInt(i);
+	        	int address=Visualizer.generateAddress();
+	        	Node newLastNode=new Node(info, address);
+	        	if (!list.isEmpty()) {
+	            		list.getLast().setNext(address); // Link the new node to the last node
+	        	}
+	        	list.addLast(newLastNode);
+	    	} 
+	    	else {
+	        	try {
+				String j=JOptionPane.showInputDialog(null, "Enter Index of new Node");
+	            		int index=Integer.parseInt(j);
+	            		if (index>=0 && index<=list.size()) {
+	                		String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
+	                		int info=Integer.parseInt(i);
+	                		int address=Visualizer.generateAddress();
+	                		Node newNode=new Node(info, address);
+	                		if(index==0) {
+	                			newNode.nextLink=Visualizer.startLink;
+	                			Visualizer.startLink=address;
+	                			list.addFirst(newNode);
+	                		}
+	                		else if(index==list.size()-1) {
+	                			list.getLast().nextLink=address;
+	                			list.addLast(newNode);
+	                		}
+	                		else {
+	                			Node current=list.get(index);
+	                			Node previous=list.get(index-1);
+	                			newNode.setNext(current.link);
+	                			previous.setNext(address);
+	                			list.add(index, newNode);
+	                		}
+	            		} 
+	            		else {
+	                		JOptionPane.showMessageDialog(null, "Index out of bounds.");
+	            		}
+	        	} 
+	        	catch (NumberFormatException e) {
+	            		JOptionPane.showMessageDialog(null, "Enter a valid integer.");
+	        	}
+	    	}
+		return list;
 	}
 	// SORT
 	public static LinkedList<Node> sortList(LinkedList<Node> list) {
