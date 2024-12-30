8ce01cd5b23992b460f502b5d0ae9ecb8b3a1111
AlBazazi <168605550+AlBazazi@users.noreply.github.com>
2024-12-30 09:06:09 +0500

    Update Operations.java

--- src/Operations.java
+++ src/Operations.java
@@ -2,112 +2,175 @@ import java.util.LinkedList;
 import javax.swing.JOptionPane;
 public class Operations {
 	// SEARCH
-	public static int searchElement(LinkedList<Node> list) {
+	public static void searchElement(LinkedList<Node> list) {
+		if(list.isEmpty()) return;
 		String strKey = JOptionPane.showInputDialog("Enter INFO to Search");
         	int key = Integer.parseInt(strKey);
-        	int index=-1;
-    		for(int i=0;i<list.size();i++) {
-    			if(list.get(i).info==key) index=i;
-    		}
-    		if(index==-1) {
-    			JOptionPane.showMessageDialog(null, "Element NOT FOUND");
-    		}
-    		else {
-    			JOptionPane.showMessageDialog(null, "Element FOUND at Index "+index);
-    		}
+        	new Thread(() -> {
+        		boolean flag=false;
+        		for (int i = 0; i < list.size(); i++) {				
+        			Node currentNode = list.get(i);
+        			currentNode.highlighted = true;
+        			Visualizer.panel.repaint();
+        			try {
+        				Thread.sleep(1000);
+        			} catch (InterruptedException e) {
+        				e.printStackTrace();
+        			}
+        			if(currentNode.info==key) {
+        				JOptionPane.showMessageDialog(null,"Element found at Index "+i,"Success",JOptionPane.INFORMATION_MESSAGE);
+        				flag=true;
+        			}
+        			currentNode.highlighted = false;
+        			Visualizer.panel.repaint();
+        			if(flag) break;
+        		}
+        		if (!flag) JOptionPane.showMessageDialog(null,"Element not Found","Failure",JOptionPane.INFORMATION_MESSAGE);
+        	}).start();
 	}
 	
 	// REMOVE
+	public static LinkedList<Node> removeElement(LinkedList<Node> list) {
+		String[] options = {"Remove from Beginning", "Remove from End", "Remove by Index"};
+	    	int choice = JOptionPane.showOptionDialog(null, "Choose Removal method:",
+	            "Remove Node", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
+	            null, options, options[0]);
+	    	if(list.isEmpty()) {
+	    		JOptionPane.showMessageDialog(null, "List is EMPTY");
+	    	}
+	    	else {
+	    		if(choice==0) {
+	    			Visualizer.startLink=list.getFirst().nextLink;
+	    			Visualizer.addresses.remove(list.getFirst().link);
+	    			list.remove(0);
+		    	}
+		    	else if(choice==1) {
+		    		if(list.size()==1) {
+		    			Visualizer.startLink=-1;
+		    			Visualizer.addresses.remove(list.getLast().link);
+		    			list.removeLast();
+		    		}
+		    		else {
+		    			list.get(list.size()-2).setNext(-1);
+		    			Visualizer.addresses.remove(list.getLast().link);
+		    			list.removeLast();
+		    		}
+		    	}
+		    	else if (choice==2){
+		    		String i=JOptionPane.showInputDialog(null, "Enter INDEX of Node");
+		        	int index=Integer.parseInt(i);
+		        	if (index<0 || index>=list.size()) {
+		        		JOptionPane.showMessageDialog(null, "Index out of bounds");
+		        	}
+		        	else {
+		        		if(index==0) {
+		        			Visualizer.startLink=list.getFirst().nextLink;
+		        			Visualizer.addresses.remove(list.getFirst().link);
+		        			list.removeFirst();
+		        		}
+		        		else if (index==list.size()-1) {
+		        			list.get(index-1).link=-1;
+		        			Visualizer.addresses.remove(list.getLast().link);
+		        			list.removeLast();
+		        		}
+		        		else {
+		        			list.get(index-1).setNext(list.get(index).nextLink);
+		        			Visualizer.addresses.remove(list.get(index).link);
+		        			list.remove(list.get(index));
+		        		}	
+		        	}
+		    	}
+		}	    	
+		return list;
+	}
 	
-	// ADD
+	// INSERT
+	// // HELPER FUNCTION
+	private static Node createNode() {
+		String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
+	        int info=Integer.parseInt(i);
+	        int address=Visualizer.generateAddress();
+	        Node node=new Node(info, address);
+		return node;
+	}
 	public static LinkedList<Node> addElement(LinkedList<Node> list) {
 		String[] options = {"Insert at Beginning", "Insert at End", "Insert by Index"};
 	    	int choice = JOptionPane.showOptionDialog(null, "Choose Insertion method:",
 	            "Insert Node", JOptionPane.DEFAULT_OPTION, JOptionPane.INFORMATION_MESSAGE,
 	            null, options, options[0]);
 	    	if (choice==0) {
-	        	String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
-	        	int info=Integer.parseInt(i);
-	        	int address=Visualizer.generateAddress();
-	        	Node newFirstNode=new Node(info, address);
+	        	Node newFirstNode= createNode();
 	        	newFirstNode.setNext(Visualizer.startLink);
-	        	Visualizer.startLink = address;
+	        	Visualizer.startLink = newFirstNode.link;
 	        	list.addFirst(newFirstNode);
 	    	}	 
 	    	else if (choice==1) {
-	        	String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
-	        	int info=Integer.parseInt(i);
-	        	int address=Visualizer.generateAddress();
-	        	Node newLastNode=new Node(info, address);
+	        	Node newLastNode=createNode();
 	        	if (!list.isEmpty()) {
-	            		list.getLast().setNext(address); // Link the new node to the last node
+	            		list.getLast().setNext(newLastNode.link); // Link the new node to the last node
 	        	}
 	        	list.addLast(newLastNode);
-	    	} 
-	    	else {
-	        	try {
-				String j=JOptionPane.showInputDialog(null, "Enter Index of new Node");
-	            		int index=Integer.parseInt(j);
-	            		if (index>=0 && index<=list.size()) {
-	                		String i=JOptionPane.showInputDialog(null, "Enter INFO of new Node");
-	                		int info=Integer.parseInt(i);
-	                		int address=Visualizer.generateAddress();
-	                		Node newNode=new Node(info, address);
-	                		if(index==0) {
-	                			newNode.nextLink=Visualizer.startLink;
-	                			Visualizer.startLink=address;
-	                			list.addFirst(newNode);
-	                		}
-	                		else if(index==list.size()-1) {
-	                			list.getLast().nextLink=address;
-	                			list.addLast(newNode);
-	                		}
-	                		else {
-	                			Node current=list.get(index);
-	                			Node previous=list.get(index-1);
-	                			newNode.setNext(current.link);
-	                			previous.setNext(address);
-	                			list.add(index, newNode);
-	                		}
-	            		} 
-	            		else {
-	                		JOptionPane.showMessageDialog(null, "Index out of bounds.");
-	            		}
-	        	} 
-	        	catch (NumberFormatException e) {
-	            		JOptionPane.showMessageDialog(null, "Enter a valid integer.");
-	        	}
-	    	}
-		return list;
+	    } 
+	    	else if (choice==2){
+			String j=JOptionPane.showInputDialog(null, "Enter Index of new Node");
+	            	int index=Integer.parseInt(j);
+	            	if (index>=0 && index<list.size()) {
+	                	Node newNode=createNode();
+	                	if(index==0) {
+	                		newNode.nextLink=Visualizer.startLink;
+	                		Visualizer.startLink=newNode.link;
+	                		list.addFirst(newNode);
+	                	}
+	                	else if(index==list.size()-1) {
+	                		list.get(index).setNext(newNode.link);
+	                		list.addLast(newNode);
+	                	}
+	                	else {
+	                		newNode.setNext(list.get(index-1).nextLink);
+	                		list.get(index-1).setNext(newNode.link);
+	                		list.add(index, newNode);
+	                	}
+	            	} 
+	            	else {
+	                	JOptionPane.showMessageDialog(null, "Index out of bounds.");
+	            	}
+	    }
+	    return list;
 	}
+	
 	// SORT
+	@SuppressWarnings("unchecked")
 	public static LinkedList<Node> sortList(LinkedList<Node> list) {
 		LinkedList<Node> sortedList = new LinkedList<>();
-       		while (!list.isEmpty()) {
-            		Node minNode = list.get(0);
+	 	LinkedList<Node> original = (LinkedList<Node>) list.clone();
+        	while (!original.isEmpty()) {
+            		Node minNode = original.get(0);
             		int index = 0;
-            		for (int i = 1; i < list.size(); i++) {
-                		if (list.get(i).info < minNode.info) {
-                    			minNode = list.get(i);
+            		for (int i = 1; i < original.size(); i++) {
+                		if (original.get(i).info < minNode.info) {
+                    			minNode = original.get(i);
                     			index = i;
                 		}
             		}
-            		list.remove(index);
+            		original.remove(index);
             		sortedList.add(minNode);
         	}
-        	// Update nextLink for sortedList
+        	if(sortedList.equals(list)) return list;
+        	// UPDATE LINKS FOR NODES
         	for (int i = 0; i < sortedList.size() - 1; i++) {
-            		sortedList.get(i).setNext(sortedList.get(i + 1).link);
+        		Node current=sortedList.get(i);
+        		Node nextNode=sortedList.get(i+1);
+        		current.setNext(nextNode.link);
         	}
-        	// Set the last node's nextLink to -1 (NULL)
-        	if (!sortedList.isEmpty()) {
-            		sortedList.getLast().setNext(-1);
-        	}
-        	Visualizer.startLink=sortedList.getFirst().link;       
+        	// SET LAST AND FIRST NODE LINKS
+        	Node lastNode=sortedList.getLast();
+        	Node firstNode=sortedList.getFirst();
+        	lastNode.link=-1;
+            	Visualizer.startLink=firstNode.link; 
 		return sortedList;
 	}
 
-	// TRAVERSE
+	// TRAVERSAL
 	public static void traverseList(LinkedList<Node> list) {
 		new Thread(() -> {
 			for (int i = 0; i < list.size(); i++) {
@@ -115,13 +178,14 @@ public class Operations {
 				currentNode.highlighted = true;
 				Visualizer.panel.repaint();
 				try {
-					Thread.sleep(1000); // 2000 milliseconds = 2 seconds
+					Thread.sleep(1000); // 1 second
 				}
 				catch (InterruptedException ex) {
 					ex.printStackTrace();
 				}
-				currentNode.highlighted = false;
+            			currentNode.highlighted = false;
             			Visualizer.panel.repaint();
-			}}).start();
+			}
+		}).start();
 	}
 }
